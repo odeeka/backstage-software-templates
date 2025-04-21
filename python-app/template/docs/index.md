26c4e521865895337fd98d155394124434cab23d
odeeka <ptibor@netbuild.hu>
2025-04-21 23:54:48 +0200

    add github python app software template

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,16 @@
+# Documents for python-app
+
+This application has two endpoints:
+
+- `python-app.test.com/api/v1/info`
+- `python-app.test.com/api/v1/helthz`
+
+Here you could expand on what each of these endpoints do.
+
+# How to access the app?
+
+You can access the app by accessing this URL: `python-app.test.com/api/v1/healthz`
+
+# This is new paragraph
+
+Test text
---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+# Endpoints
+#'/api/v1/details'
+#'/api/v1/healthz'
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hello World from root'
+
+@app.route('/api/v1/details')
+def details():
+  # return '<h1>Hello World (details)</h1>'
+  return jsonify({
+    'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+    'hostname': socket.gethostname(),
+    'message': 'You are great in Python and Github Action and Kubernetes and DevOps and GitOps and Cloud and Linux'
+  })
+
+@app.route('/api/v1/healthz')
+def health():
+  return jsonify({'status':'up'})
+
+if __name__ == '__main__':
+  # Need to set the host for reaching the docker container from host
+  app.run(host="0.0.0.0")
